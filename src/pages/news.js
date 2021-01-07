6f24b285b418834d74a24341e3684b959dce1054
aditya rahmayadi <adityarahmayadi@gmail.com>
2021-01-07 22:58:26 +0700

    add news page - add news page - add user.json

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import { Box, Text, Image } from 'goods-core'
+import data from '@xendit/data/users.json'
 
-const NewsPage = () => <h4>News Page</h4>
-
+const NewsPage = () => {
+  return (
+    <Box w p={{ xs: '0px 16px', lg: '0px 120px', xl: '0px 240px' }}>
+      {data.data.map((item) => (
+        <Box
+          key={item.source.key}
+          w
+          p='16px'
+          shadow='5px 8px 24px 5px rgba(208, 216, 243, 0.6)'
+          radius='8px'
+        >
+          <Image src={item.urlToImage} objectFit='contain' mb='32px' />
+          <Text as='h1' fSize={{ xs: '16px', lg: '32px' }}>
+            {item.title}
+          </Text>
+          <Text mb='8px' fSize='12px'>
+            {item.description}
+          </Text>
+          <Text>{item.content}</Text>
+        </Box>
+      ))}
+    </Box>
+  )
+}
 export default NewsPage
